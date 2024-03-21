20c9acb09831ad27a540afae798671787f6bb6fb
David Andersson <lejonmanen@gmail.com>
2024-03-21 11:19:57 +0100

    AddFriend cleanup

--- src/routes/AddFriend.jsx
+++ src/routes/AddFriend.jsx
@@ -2,20 +2,19 @@ import { useFriendStore } from "../data/store.js"
 import { useState } from 'react'
 
 const AddFriend = () => {
+	// ** state-variabler, lokala och hämtade från store ** //
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
 
-	const { friends, newFriend } = useFriendStore(state => ({
-		friends: state.friends,  // TODO: ta bort när vi är klara
-		newFriend: state.newFriend
-	}))
+	const newFriend = useFriendStore(state => state.newFriend)
 	// Alternativ:
-	// const friends = useFriendStore(state => state.friends)
-	// const newFriend = useFriendStore(state => state.newFriend)
+	// const { friends, newFriend } = useFriendStore(state => ({
+	// 	friends: state.friends,  // Användes för felsökning
+	// 	newFriend: state.newFriend
+	// }))
 
-	console.log('AddFriend friends = ', friends)
-	// console.log('AddFriend name, email: ', name, email);
 
+	// ** Event-funktioner ** //
 	// Så att state-variablerna uppdateras när man skriver
 	const handleNameChange = event => {
 		setName(event.target.value)
@@ -26,8 +25,11 @@ const AddFriend = () => {
 
 	const doSaveFriend = () => newFriend(name, email)
 
+
+	// ** Variabler och konstanter som stöd för JSX ** //
 	// Räknas ut baserat på state-variablerna varje gång komponenten renderas
 	const canSaveFriend = name !== '' && email !== ''
+
 
 	return (
 		<section className="form">
