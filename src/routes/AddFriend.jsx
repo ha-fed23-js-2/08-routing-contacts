0df339527b6355e0fef668ea7e30c980361de2b8
David Andersson <lejonmanen@gmail.com>
2024-03-21 10:37:22 +0100

    AddFriend done

--- src/routes/AddFriend.jsx
+++ src/routes/AddFriend.jsx
@@ -1,8 +1,12 @@
 import { useFriendStore } from "../data/store.js"
+import { useState } from 'react'
 
 const AddFriend = () => {
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+
 	const { friends, newFriend } = useFriendStore(state => ({
-		friends: state.friends,
+		friends: state.friends,  // TODO: ta bort när vi är klara
 		newFriend: state.newFriend
 	}))
 	// Alternativ:
@@ -10,12 +14,39 @@ const AddFriend = () => {
 	// const newFriend = useFriendStore(state => state.newFriend)
 
 	console.log('AddFriend friends = ', friends)
+	// console.log('AddFriend name, email: ', name, email);
+
+	// Så att state-variablerna uppdateras när man skriver
+	const handleNameChange = event => {
+		setName(event.target.value)
+	}
+	const handleEmailChange = event => {
+		setEmail(event.target.value)
+	}
+
+	const doSaveFriend = () => newFriend(name, email)
+
+	// Räknas ut baserat på state-variablerna varje gång komponenten renderas
+	const canSaveFriend = name !== '' && email !== ''
 
 	return (
 		<section className="form">
-			<div> <input type="text" defaultValue="Bosse Bengtsson" /> </div>
-			<div> <input type="text" defaultValue="bosse@mail.com" /> </div>
-			<button> Spara </button>
+			<div>
+				<label> Namn </label>
+				<input type="text"
+					onChange={handleNameChange}
+					/>
+			</div>
+			<div>
+				<label> E-post </label>
+				<input type="text"
+					onChange={handleEmailChange}
+					/>
+			</div>
+			<button
+				onClick={doSaveFriend}
+				disabled={!canSaveFriend}
+				> Spara </button>
 		</section>
 	)
 }
